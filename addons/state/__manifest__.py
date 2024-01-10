5d0413b567902af3d6211409e9c04da6f3ccb717
Nogyboy <guamanc9@gmail.com>
2024-01-10 12:11:57 -0500

    feat: Add real estate module

---
+++ addons/state/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Inmobiliaria",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Christian Guaman",
+    'description': """
+    Módulo de inmobiliaria
+    """,
+    'application': True,
+}
