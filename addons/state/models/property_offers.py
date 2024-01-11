f51aaba04016b8e83b69551bc5850f2feb85a7b5
Nogyboy <guamanc9@gmail.com>
2024-01-11 10:21:54 -0500

    feat: Add action to accept and refuse

--- addons/state/models/property_offers.py
+++ addons/state/models/property_offers.py
@@ -1,15 +1,18 @@
 from odoo import api, models, fields
 from datetime import timedelta
 
+from odoo.exceptions import UserError
+
 class PropertyOffer(models.Model):
     _name = "estate_property_offer"
     _description = "Real Estate Property Offers Model"
 
     price = fields.Float(required=True, string="Precio")
     status = fields.Selection([
+        ('draft', 'Draft'),
         ('accepted', 'Accepted'),
         ('refused', 'Refused'),
-    ], default='accepted', required=True, string="Estado", copy=False)
+    ], default='draft', required=True, string="Estado", copy=False,)
     partner_id = fields.Many2one('res.partner', string="Comprador", required=True)
     property_id = fields.Many2one('estate_property', string="Propiedad", required=True)
     validity = fields.Integer(string="Validez (días)", default=7)
@@ -17,13 +20,17 @@ class PropertyOffer(models.Model):
 
     @api.depends('validity')
     def _compute_date_deadline(self):
-        self.date_deadline = fields.Date.today() + timedelta(days=self.validity)
+            for record in self:
+                record.date_deadline = fields.Date.today() + timedelta(days=record.validity)
     
     def _inverse_date_deadline(self):
         for record in self:
             record.validity = (record.date_deadline - fields.Date.today()).days
     
     def action_confirm_offer(self):
+        # Validate if any other offer has been accepted
+        if self.property_id.offer_ids.filtered(lambda r: r.status == 'accepted'):
+            raise UserError("Ya existe una oferta aceptada para esta propiedad.")
         # Set offer price as selling price
         self.property_id.selling_price = self.price
         # Set property state to offer_accepted
@@ -31,6 +38,9 @@ class PropertyOffer(models.Model):
         return True
     
     def action_refuse_offer(self):
+        # Validate if this offer was canceled
+        if self.status == 'refused' or self.status == 'accepted':
+            raise UserError("Esta oferta ya ha sido rechazada o aceptada.")
         # Refuse offer
         self.status = 'refused'
         return True
