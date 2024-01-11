ca1a13118945b403db3343d1a95ff1b72c8a86c7
Nogyboy <guamanc9@gmail.com>
2024-01-11 08:42:25 -0500

    feat: Add inverse compute

--- addons/state/models/property_offers.py
+++ addons/state/models/property_offers.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import api, models, fields
+from datetime import timedelta
 
 class PropertyOffer(models.Model):
     _name = "estate_property_offer"
@@ -11,3 +12,13 @@ class PropertyOffer(models.Model):
     ], default='accepted', required=True, string="Estado", copy=False)
     partner_id = fields.Many2one('res.partner', string="Comprador", required=True)
     property_id = fields.Many2one('estate_property', string="Propiedad", required=True)
+    validity = fields.Integer(string="Validez (días)", default=7)
+    date_deadline = fields.Date(string="Fecha límite", compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        self.date_deadline = fields.Date.today() + timedelta(days=self.validity)
+    
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - fields.Date.today()).days
